b623cced3984c7f136ea4d2350089daed3829fe9
Samim Hossain Mondal <122428887+samim-hossain-mondal@users.noreply.github.com>
2023-12-22 12:11:14 +0530

    AI model created GAIJ2C-269.py

---
+++ GAIJ2C-269.py
@@ -0,0 +1,3 @@
+You can contact customer support by sending an email to <EMAIL>.
+
+Note: The contact form is not included in the theme. You can use a third-party plugin or create your own contact form.
